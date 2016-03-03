741dba6ef07ea9a52d6a12d9a1c43af510e60e4c
Josh <JoshRickers@users.noreply.github.com>
2016-03-03 16:06:50 +0000

    Main function, help update

--- LogParse.py
+++ LogParse.py
@@ -4,8 +4,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-l', action='store', dest='log_file', help='Log file', required=True)
 parser.add_argument('-o', action='store', dest='out_file', help='Output file', required=True)
 parser.add_argument('-s', action='store', dest='search_term', help='Search term')
-parser.add_argument('-t', action='store', dest='time', help='time')
-parser.add_argument('-r', action='store', dest='timeRange', nargs=2, help='Enter two time')
+parser.add_argument('-t', action='store', dest='time', help='time in format hh:mm')
+parser.add_argument('-r', action='store', dest='timeRange', nargs=2, help='Enter two times for to set a range in format hh:mm')
 
 args = parser.parse_args()
 
@@ -74,19 +74,22 @@ def searchTimeRangeString(timeRange, string, outfile):
 	of.close()
 	return result
 
-if args.time and args.search_term:
-	if not searchTimeString(args.time, args.search_term, args.out_file):
-		print('No Results')
-elif args.timeRange and args.search_term:
-	if not searchTimeRangeString(args.timeRange, args.search_term, args.out_file):
-		print('No Results')
-elif args.timeRange:
-	if not searchTimeRange(args.timeRange, args.out_file):
-		print('No Results')
-elif args.time:
-	if not searchTime(args.time, args.out_file):
-		print('No Results')
-elif args.search_term:
-	if not searchString(args.search_term, args.out_file):
-		print('No Results')
+def main():
+	if args.time and args.search_term:
+		if not searchTimeString(args.time, args.search_term, args.out_file):
+			print('No Results')
+	elif args.timeRange and args.search_term:
+		if not searchTimeRangeString(args.timeRange, args.search_term, args.out_file):
+			print('No Results')
+	elif args.timeRange:
+		if not searchTimeRange(args.timeRange, args.out_file):
+			print('No Results')
+	elif args.time:
+		if not searchTime(args.time, args.out_file):
+			print('No Results')
+	elif args.search_term:
+		if not searchString(args.search_term, args.out_file):
+			print('No Results')
 
+if __name__ == "__main__":
+	main()
