0f6879e81d5e996c9380f8842b3c91dbf9944bcd
Josh <JoshRickers@users.noreply.github.com>
2016-03-03 15:51:53 +0000

    time range and string search
    Added a time range and string search so you can search for a time range and also a string within that time range.

--- LogParse.py
+++ LogParse.py
@@ -58,8 +58,27 @@ def searchTimeRange(timeRange, outfile):
 	of.close()
 	return result
 
+def searchTimeRangeString(timeRange, string, outfile):
+	of = open(outfile, 'a')
+	inRange = False
+	result = False
+	with open(args.log_file) as fp:
+		for line in fp:
+			if timeRange[0] in line[17:22]:
+				inRange = True
+			if timeRange[1] in line[17:22]:
+				inRange = False
+			if inRange and string in line:
+				of.write(line)
+				result = True
+	of.close()
+	return result
+
 if args.time and args.search_term:
 	if not searchTimeString(args.time, args.search_term, args.out_file):
+		print('No Results')
+elif args.timeRange and args.search_term:
+	if not searchTimeRangeString(args.timeRange, args.search_term, args.out_file):
 		print('No Results')
 elif args.timeRange:
 	if not searchTimeRange(args.timeRange, args.out_file):
